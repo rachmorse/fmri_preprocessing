4546462fd3ee0a886385108d2245a57f713200e5
rachmorse <rachel.morse.20@ucl.ac.uk>
2025-12-10 15:50:15 +0100

    Added comments, cleaned script

--- src/preprocessing/bold_nuisance_correction_wf.py
+++ src/preprocessing/bold_nuisance_correction_wf.py
@@ -1,66 +1,62 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#
-# creates a nuisance matrix and regresses the fMRI voxels on it and keeps the
-# residuals
+"""
+This script defines the workflow for nuisance regression in fMRI data.
+It includes steps for motion correction, cosine filtering, and CompCor (WM/CSF) regression.
+"""
 
 
 def motion_regressors(realign_movpar_txt, output_dir, order=0, derivatives=1):
-    """Compute motion regressors upto given order and derivative
-    motion + d(motion)/dt + d2(motion)/dt2 (linear + quadratic)
-    Parameters
-    ----------
-    realign_movpar_txt: [string] file path where realignment's motion parameters are
-    output_dir: [string] directory path where the output is gonna be saved
-    order:
-    derivatives:
+    """Compute motion regressors up to a given order and derivative.
+
+    Calculates motion + d(motion)/dt + d2(motion)/dt2 (linear + quadratic).
+
+    Args:
+        realign_movpar_txt (str): File path to the realignment motion parameters.
+        output_dir (str): Directory path where the output will be saved.
+        order (int): Order of the polynomial expansion. Defaults to 0.
+        derivatives (int): Number of derivatives to compute. Defaults to 1.
 
     Returns:
-    -------
-    filename:
+        str: The file path of the generated motion regressors text file.
     """
     import os
-
     import numpy as np
 
     params = np.genfromtxt(realign_movpar_txt)
     out_params = params
-    # TODO: why does this need to be done repeatedly? Given that you only reference the outparams once outside of the
-    # loop?
+
     for d in range(1, derivatives + 1):
-        # TODO: this would repeat d times - what does this actually do? Why? Explain this better in code
+        # Create a lagged version of the parameters to compute differences
         # Duplicates first row and put it on the top of the matrix
         cparams = np.vstack((np.repeat(params[0, :][None, :], d, axis=0), params))
 
-        # TODO: This is all really unclean, and hard to follow. Why do you need the first 6 rows only?
-        # Computes the difference of every row and its posterior: row(n) - row(n-1)
-        # and appends it as 6 new columns on the right
+        # Calculate the difference between the current row and the previous ones (derivatives)
+        # We take the first 6 columns (original motion parameters) and center them
         out_params = out_params[:, 0:6] - out_params[0, 0:6] * np.ones([out_params.shape[0], 1])
         out_params = np.hstack((out_params, np.diff(cparams, d, axis=0)))
 
-    out_params2 = out_params
-    # We will never use second order
-    # for i in range(1, order + 1):
-    # out_params2 = np.hstack((out_params2, np.power(out_params, i)))
+    # Note: Higher order expansions (e.g., quadratic) are currently not used/implemented
+    # but the structure allows for it if 'order' > 0 loop is uncommented/implemented.
+    
     filename = os.path.join(output_dir, "motion_regressor.txt")
-    np.savetxt(filename, out_params2, fmt="%.10f")
+    np.savetxt(filename, out_params, fmt="%.10f")
     return filename
 
 
 def cosine_filter_txt(timepoints, timestep, output_dir, period_cut=128):
-    """Creates the discrete cosine transform (DCT) basis functions
-    Parameters
-    ----------
-    period_cut:  minimum period for the cosine basis functions
-    timestep : must be equal to the fmri TR
-    timepoints: number of time volumes (length of the acquisition)
+    """Create the discrete cosine transform (DCT) basis functions for high-pass filtering.
+
+    Args:
+        timepoints (int): Number of time volumes (length of the acquisition).
+        timestep (float): The fMRI TR (Repetition Time) in seconds.
+        output_dir (str): Directory path where the output will be saved.
+        period_cut (float): Minimum period for the cosine basis functions in seconds. Defaults to 128.
 
     Returns:
-    -------
-    filename:
+        str: The file path of the generated cosine filter text file.
     """
     import os
-
     import nipype.algorithms.confounds as cf
     import numpy as np
 
@@ -73,24 +69,27 @@ def cosine_filter_txt(timepoints, timestep, output_dir, period_cut=128):
 
 
 def get_nuisance_regressors_wf(outdir, timepoints, subject_id, global_signal=False, order=0, derivatives=1, comp=3):
-    """Creates nipype workflow for nuisance correction composed by:
-        Intercept + Drift (cosine transform) + Motion Correction + WhiteMatter&CSF Nuisance Regressors (CompCor)
-    Parameters
-    ----------
-    outdir:
-    subject_id:
-    global_signal:
-    timepoints:
-    order:
-    derivatives:
-    comp:
+    """Create a Nipype workflow for nuisance correction.
+
+    The workflow includes:
+    1. Intercept
+    2. Drift (cosine transform)
+    3. Motion Correction parameters
+    4. White Matter & CSF Nuisance Regressors (aCompCor)
+
+    Args:
+        outdir (str): Output directory for the workflow.
+        timepoints (int): Number of time volumes.
+        subject_id (str): Subject identifier.
+        global_signal (bool): Whether to include global signal regression. Defaults to False.
+        order (int): Order of motion parameter expansion. Defaults to 0.
+        derivatives (int): Number of motion parameter derivatives. Defaults to 1.
+        comp (int): Number of CompCor components to extract. Defaults to 3.
 
     Returns:
-    -------
-    wf_reg:
+        nipype.pipeline.engine.Workflow: The configured nuisance regression workflow.
     """
     import os
-
     from nipype import Node, Workflow
     from nipype.algorithms import confounds
     from nipype.interfaces import fsl, utility
@@ -105,32 +104,34 @@ def get_nuisance_regressors_wf(outdir, timepoints, subject_id, global_signal=Fal
     print("Setting INPUT node...")
     node_input = Node(
         utility.IdentityInterface(
-            fields=["realign_movpar_txt", "rfmri_unwarped_imgs", "mask_wm", "mask_csf", "global_mask_img", "bold_img"]
+            fields=[
+                "realign_movpar_txt",
+                "rfmri_unwarped_imgs",
+                "mask_wm",
+                "mask_csf",
+                "global_mask_img",
+                "bold_img",
+            ]
         ),
         name="input_node",
     )
 
-    # Merging wm and csf masks
+    # Node to merge WM and CSF masks for CompCor
     node_merge_wm_csf = Node(utility.base.Merge(2), name="Merge_wm_csf")
 
-    # AcompCor
+    # Node for Anatomical CompCor (aCompCor)
+    # Extracts principal components from WM and CSF masks to use as nuisance regressors
     node_ACompCor = Node(
         confounds.ACompCor(
-            num_components=3,
-            # save_pre_filter='high_pass_filter.txt',
+            num_components=comp,
             pre_filter=False,
-            # high_pass_cutoff=128,
             repetition_time=0.8,
             merge_method="none",
-            # use_regress_poly=False,
-            # realigned_file= fMRI_BOLD_unwarped,
-            # mask_files='/institut/processed_data/BBHI_func/output2/sub-41064/GetMasksInT1Space/binarize_mask/MNI152_WM_09_warp_thresh.nii.gz',
         ),
         name="AcompCor_mask",
     )
-    # node_ACompCor.inputs.save_pre_filter=os.path.join(os.path.join(os.path.join(wf_reg.base_dir,wf_reg.name),node_ACompCor.name), 'high_pass_filter.txt')
 
-    # cosine_filter
+    # Node for creating Cosine Filter (High-pass filter) regressors
     node_cosine_filter_reg = Node(
         utility.Function(
             input_names=["timepoints", "timestep", "period_cut", "output_dir"],
@@ -144,18 +145,8 @@ def get_nuisance_regressors_wf(outdir, timepoints, subject_id, global_signal=Fal
     )
     node_cosine_filter_reg.inputs.timepoints = timepoints
     node_cosine_filter_reg.inputs.timestep = 0.8
-    # node_cosine_filter_reg.overwrite=True
 
-    # global_signal
-    #    if global_signal :
-    #        node_global_signal=Node(utility.Function(input_names=["timeseries_file", "label_file", "filename"],
-    #                                 output_names=["global_signal_txt"],
-    #                                 function=extract_subrois),
-    #                                    name="global_signal")
-    #        node_global_signal.inputs.filename=os.path.join(os.path.join(os.path.join(os.path.join(wf_reg.base_dir,wf_reg.name)),node_global_signal.name),'global_signal.txt')
-    #        #node_global_signal.overwrite=True
-
-    # motion regressors
+    # Node for calculating Motion Regressors (and derivatives)
     motion_regressors_interface = utility.Function(
         input_names=["realign_movpar_txt", "output_dir", "order", "derivatives"],
         output_names=["motion_reg_txt"],
@@ -165,11 +156,11 @@ def get_nuisance_regressors_wf(outdir, timepoints, subject_id, global_signal=Fal
     node_motion_regressors.inputs.output_dir = os.path.join(
         os.path.join(os.path.join(wf_reg.base_dir, wf_reg.name)), node_motion_regressors.name
     )
-    # node_motion_regressors.overwrite=True
 
-    # merges all regressors
+    # Node to merge all nuisance text files into one list
     node_merge_txts = Node(utility.base.Merge(4), name="Merge_txt_inputs")
 
+    # Node to combine all regressors into a single matrix file
     node_merge_regressors = Node(
         utility.Function(
             input_names=["nuisance_txts", "output_dir"],
@@ -182,18 +173,17 @@ def get_nuisance_regressors_wf(outdir, timepoints, subject_id, global_signal=Fal
         os.path.join(wf_reg.base_dir, wf_reg.name), node_merge_regressors.name
     )
 
+    # Node to apply the regression to the BOLD data
     node_filter_regressor = Node(
         fsl.FilterRegressor(
-            # design_file (-d) nuissance_txt
             filter_all=True,
-            # in_file (-i) bold after SPM coregistration to T1
-            # out_file
         ),
         name="filter_regressors_bold",
     )
 
     node_output = Node(utility.IdentityInterface(fields=["nuisance_txt", "bold_nuisance_filtered"]), name="output_node")
 
+    # Connect the nodes in the workflow
     wf_reg.connect([
         (node_input, node_merge_wm_csf, [("mask_wm", "in1"), ("mask_csf", "in2")]),
         (node_input, node_ACompCor, [("rfmri_unwarped_imgs", "realigned_file")]),
@@ -204,12 +194,6 @@ def get_nuisance_regressors_wf(outdir, timepoints, subject_id, global_signal=Fal
         (node_cosine_filter_reg, node_merge_txts, [("cosine_filter_txt", "in3")]),
         (node_merge_txts, node_merge_regressors, [("out", "nuisance_txts")]),
     ])
-    #    if global_signal:
-    #         wf_reg.connect([
-    #                         (node_input, node_global_signal,[('rfmri_unwarped_imgs', 'timeseries_file'),
-    #                                                     ('global_mask_img', 'label_file')]),
-    #                        (node_global_signal, node_merge_txts, [('global_signal_txt', 'in4')])
-    #                         ])
 
     wf_reg.connect([
         (node_merge_regressors, node_filter_regressor, [("nuisance_txt", "design_file")]),
@@ -217,63 +201,37 @@ def get_nuisance_regressors_wf(outdir, timepoints, subject_id, global_signal=Fal
         (node_filter_regressor, node_output, [("out_file", "bold_nuisance_filtered")]),
         (node_merge_regressors, node_output, [("nuisance_txt", "nuisance_txt")]),
     ])
+
     return wf_reg
 
 
-# def extract_subrois(timeseries_file, label_file, filename,indices=[1]):
-#    """Extract voxel time courses for each subcortical roi index
-#    Parameters
-#    ----------
-#    timeseries_file: a 4D Nifti file
-#    label_file: a 3D file containing rois in the same space/size of the 4D file
-#    indices: a list of indices for ROIs to extract.
-#
-#    Returns
-#    -------
-#    out_file: a text file containing time courses for each voxel of each roi
-#        The first four columns are: freesurfer index, i, j, k positions in the
-#        label file
-#    """
-#    import nibabel as nb
-#    from nipype.utils import NUMPY_MMAP, filemanip
-#    import os
-#    import numpy as np
-#
-#    img = nb.load(timeseries_file, mmap=NUMPY_MMAP)
-#    data = img.get_data()
-#    roiimg = nb.load(label_file, mmap=NUMPY_MMAP)
-#    rois = roiimg.get_data()
-#    prefix = filemanip.split_filename(timeseries_file)[1]
-#    out_ts_file = os.path.join(os.getcwd(), 'img.nii.gz' )
-#   # with open(out_ts_file, 'wt') as fp:
-#    data2=data.copy()
-#    data_txt=[]
-#    for fsindex in indices:
-#        ijk = np.nonzero(rois == fsindex)
-#        data2[ijk]=2
-#        ts = np.mean(data[ijk], axis=0)
-#        data_txt.append(ts)
-#    t_means=np.hstack(data_txt)
-#    np.savetxt(filename, t_means, fmt=b"%.10f")
-#    return filename
-
-
 def merge_nuisance_regressors(nuisance_txts, output_dir, standardize=True):
-    # https://www.ncbi.nlm.nih.gov/pubmed/30666750
-    # Serial filtering introduces spurious correlations: SOLUTIONS:
-    # (a) combining all steps into a single linear filter, or (b) sequential orthogonalization of covariates/linear filters performed in series.
-    # our procedure is a)
-    import os
+    """Merge multiple nuisance regressor files into a single matrix.
 
+    This function combines motion parameters, CompCor components, and cosine drift terms.
+    It also standardizes the regressors (z-score) to avoid scaling issues.
+    
+    References:
+        https://www.ncbi.nlm.nih.gov/pubmed/30666750
+        (Combining all steps into a single linear filter)
+
+    Args:
+        nuisance_txts (list): List of file paths to the nuisance text files.
+        output_dir (str): Directory path where the output will be saved.
+        standardize (bool): Whether to standardize (z-score) the regressors. Defaults to True.
+
+    Returns:
+        str: The file path of the combined nuisance regressors text file.
+    """
+    import os
     import numpy as np
 
     out_files = []
-    # TODO: You're just making all these files then reading them in the next function. Why doing so much read and write?
-    # TODO: consider passing the data directly
-    # FYI: if you have long for loops and you want to know the progress, you can use tqdm
     txts_values = []
     for txt in nuisance_txts:
         txt_values = np.genfromtxt(txt)
+        
+        # Handle 1D arrays (single regressor)
         if txt_values.ndim == 1:
             txt_values = np.matrix(txt_values)
             txt_values = txt_values[~np.isnan(txt_values)].transpose()
@@ -281,6 +239,7 @@ def merge_nuisance_regressors(nuisance_txts, output_dir, standardize=True):
                 print("hy")
                 txt_values = (txt_values - txt_values.mean(axis=0)) / np.std(txt_values, axis=0)
             txts_values.append(txt_values)
+        # Handle 2D arrays (multiple regressors)
         else:
             txt_values = txt_values[~np.isnan(txt_values).any(axis=1)]
             txt_values = np.matrix(txt_values)
@@ -288,11 +247,14 @@ def merge_nuisance_regressors(nuisance_txts, output_dir, standardize=True):
                 print("h")
                 txt_values = (txt_values - txt_values.mean(axis=0)) / np.std(txt_values, axis=0)
             txts_values.append(txt_values)
+    
         out_params = np.hstack((txts_values))
 
-    # adds column of ones
+    # Add a column of ones (intercept)
     out_params = np.hstack((np.ones((len(out_params), 1)), out_params))
+    
     filename = os.path.join(output_dir, "all_nuisances.txt")
     print(filename)
     np.savetxt(filename, out_params, fmt="%.10f")
     return filename
+
